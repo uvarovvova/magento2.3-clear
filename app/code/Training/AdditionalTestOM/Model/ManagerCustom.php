d97eec6bce288c6200942c9c3530dcc8a5ed498a
vova <uvarov.vova@gmail.com>
2019-02-03 01:02:29 +0200

    1.3 part

---
+++ app/code/Training/AdditionalTestOM/Model/ManagerCustom.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Training\AdditionalTestOM\Model;
+
+/**
+ * Class ManagerCustom
+ * @package Training\AdditionalTestOM\Model
+ */
+class ManagerCustom
+{
+
+	public function create()
+	{
+	}
+
+	public function get()
+	{
+	}
+}
