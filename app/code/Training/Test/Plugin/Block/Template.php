83a825205deb124bc09f0605ed2db8c77a72cdec
vova <uvarov.vova@gmail.com>
2019-02-07 22:20:26 +0200

    1.9

--- app/code/Training/Test/Plugin/Block/Template.php
+++ app/code/Training/Test/Plugin/Block/Template.php
@@ -8,12 +8,12 @@ class Template
 
 	public function afterToHtml(
 		\Magento\Framework\View\Element\Template $subject,
-		$result = null
-	)
-	{
-		$result = '<div><p>' . $subject->getTemplate() . '</p>'
-			. '<p>' . get_class($subject) . '</p>' . $result . '</div>';
-
-		return $result;
+		$result
+	) {
+		if ($subject->getNameInLayout() == 'top.search') {
+			$result = '<div><p>' . $subject->getTemplate() . '</p>'
+				. '<p>' . get_class($subject) . '</p>' . $result . '</div>';
+			return $result;
+		}
 	}
 }
