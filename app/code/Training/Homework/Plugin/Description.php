0526bacff4d458dffed613f97dcb3933019f2c9e
vova <uvarov.vova@gmail.com>
2019-02-20 21:08:00 +0200

    3.7

--- app/code/Training/Homework/Plugin/Description.php
+++ app/code/Training/Homework/Plugin/Description.php
@@ -4,10 +4,19 @@ namespace Training\Homework\Plugin;
 
 use Magento\Catalog\Block\Product\View\Description as CoreDescription;
 
+/**
+ * Class Description
+ * @package Training\Homework\Plugin
+ */
 class Description
 {
+	/**
+	 * @param CoreDescription $subject
+	 */
 	public function beforeToHtml(CoreDescription $subject)
 	{
-		$subject->getProduct()->setDescription('Test description');
+//		$subject->getLayout()->createBlock(\Training\Homework\Block\Test::class);
+		$subject->setTemplate('Training_Homework::description.phtml');
+
 	}
 }
