1b69b2e419b87454f3b819e32f69d5222429d95d
vova <uvarov.vova@gmail.com>
2019-02-27 22:09:59 +0200

    4.3

---
+++ app/code/Training/JsHomework/view/frontend/web/js/checkout/printed-invoice.js
@@ -0,0 +1,14 @@
+define([
+    'uiComponent',
+    'jquery',
+    'mage/mage',
+    'mage/decorate'
+], function (Component, $) {
+    'use strict';
+
+    return Component.extend({
+	    // defaults: {
+		 //    template: 'Training_JsHomework/checkout/printed-invoice'
+	    // }
+    });
+});
