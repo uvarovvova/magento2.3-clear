d96837d5460936e0f04ca410a2349613b8ac4fa0
vova <uvarov.vova@gmail.com>
2019-03-08 13:15:08 +0200

    5.2

--- app/code/Training/Feedback/Block/FeedbackList.php
+++ app/code/Training/Feedback/Block/FeedbackList.php
@@ -8,6 +8,7 @@ use Magento\Framework\View\Element\Template;
 use Magento\Framework\View\Element\Template\Context;
 use Training\Feedback\Model\ResourceModel\Feedback\CollectionFactory;
 use Magento\Framework\DataObject;
+use Training\Feedback\Model\ResourceModel\Feedback;
 
 /**
  * Class FeedbackList
@@ -16,20 +17,46 @@ use Magento\Framework\DataObject;
 class FeedbackList extends Template
 {
 	const PAGE_SIZE = 5;
+
+	/**
+	 * @var CollectionFactory
+	 */
 	private $collectionFactory;
-	private $collection;
+
+	/**
+	 * @var Timezone
+	 */
 	private $timezone;
 
+
+	private $collection;
+
+	/**
+	 * @var Feedback
+	 */
+	private $feedbackResource;
+
+
+	/**
+	 * FeedbackList constructor.
+	 * @param Context $context
+	 * @param CollectionFactory $collectionFactory
+	 * @param Timezone $timezone
+	 * @param Feedback $feedbackResource
+	 * @param array $data
+	 */
 	public function __construct(
 		Context $context,
 		CollectionFactory $collectionFactory,
 		Timezone $timezone,
+		Feedback $feedbackResource,
 		array $data = array()
 	)
 	{
 		parent::__construct($context, $data);
 		$this->collectionFactory = $collectionFactory;
 		$this->timezone = $timezone;
+		$this->feedbackResource = $feedbackResource;
 	}
 
 	/**
@@ -88,5 +115,21 @@ class FeedbackList extends Template
 	public function getFeedbackDate($feedback)
 	{
 		return $this->timezone->formatDateTime($feedback->getCreationTime());
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getAllFeedbackNumber()
+	{
+		return $this->feedbackResource->getAllFeedbackNumber();
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getActiveFeedbackNumber()
+	{
+		return $this->feedbackResource->getActiveFeedbackNumber();
 	}
 }
--- app/code/Training/Feedback/Model/ResourceModel/Feedback.php
+++ app/code/Training/Feedback/Model/ResourceModel/Feedback.php
@@ -2,13 +2,42 @@
 
 namespace Training\Feedback\Model\ResourceModel;
 
-
 use Magento\Framework\Model\ResourceModel\Db\AbstractDb;
 
+/**
+ * Class Feedback
+ * @package Training\Feedback\Model\ResourceModel
+ */
 class Feedback extends AbstractDb
 {
+	/**
+	 * Init
+	 */
 	protected function _construct()
 	{
 		$this->_init('training_feedback', 'feedback_id');
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getAllFeedbackNumber()
+	{
+		$adapter = $this->getConnection();
+		$select = $adapter->select()
+			->from('training_feedback', new \Zend_Db_Expr('COUNT(*)'));
+		return $adapter->fetchOne($select);
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getActiveFeedbackNumber()
+	{
+		$adapter = $this->getConnection();
+		$select = $adapter->select()
+			->from('training_feedback', new \Zend_Db_Expr('COUNT(*)'))
+			->where('is_active = ?', \Training\Feedback\Model\Feedback::STATUS_INACTIVE);
+		return $adapter->fetchOne($select);
 	}
 }
